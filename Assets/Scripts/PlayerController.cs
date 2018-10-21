975f74d1018d107452bd451fadbbfbfe98aaf8ae
Renārs Grebežs <originalchc@gmail.com>
2018-10-21 23:49:18 +0200

    First commit
    It works.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text scoreText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int score;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        score = 0;
+        SetScoreText();
+        winText.text = "";
+    }
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            score++;
+            SetScoreText();
+        }
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+
+        if (score >= 6)
+        {
+            winText.text = "YOU WON!";
+        }
+    }
+}
